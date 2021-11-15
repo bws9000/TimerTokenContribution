b18a9bae386902a75c4bf03d75a857064c4b0654
bws9000 <snyder.burt@gmail.com>
2021-11-15 16:24:44 -0500

    unused variables

--- contracts/Contribute.sol
+++ contracts/Contribute.sol
@@ -6,16 +6,13 @@ import "hardhat/console.sol";
 
 contract Contribute {
     event Received(address, uint256);
-    event Approval(address, address, uint256);
 
-    address TimerTokenAddress;
-    mapping(address => mapping(address => uint256)) private allowed;
-    mapping(address => uint256) private balances;
-    TimerToken public token;
+    // address TimerTokenAddress;
+    // TimerToken public token;
 
     constructor(address _tokenAddress) {
-        TimerTokenAddress = _tokenAddress;
-        token = TimerToken(TimerTokenAddress);
+        // TimerTokenAddress = _tokenAddress;
+        // token = TimerToken(TimerTokenAddress);
     }
 
     receive() external payable {
@@ -23,7 +20,6 @@ contract Contribute {
         emit Received(msg.sender, msg.value);
     }
 
-    // Fallback function is called when msg.data is not empty
     fallback() external payable {}
 
     function getBalance() public view returns (uint256) {
